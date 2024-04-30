1a63880eda670c0d320acea0caafa56e5997f419
John DoeDesu <johndoe@example.com>
2024-04-30 18:15:37 +0900

    クライアントの接続処理を追加#1

--- ModelLayer/Model.cs
+++ ModelLayer/Model.cs
@@ -30,6 +30,32 @@ namespace ReMVC_Study
             {
                 LogTxtBox_Model.AppendText($"SocketException:{ex}\r\n") ;
             }
+
+            Task.Run(() => 
+            {
+                try
+                {
+                    // UI更新
+                    Invoke(() => 
+                    {
+                        LogTxtBox_Model.AppendText("Waiting for a connection... \r\n");
+                    });
+
+                    // クライアントの接続処理(AcceptTcpClientのおかげでボタン連打でフリーズするのを回避している)
+                    // クライアントの接続をブロッキング（処理を停止して待つ）モードで受け入れ
+                    using (TcpClient client = _server.AcceptTcpClient())
+                    {
+
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            
+            });
+
         }
     }
 }
